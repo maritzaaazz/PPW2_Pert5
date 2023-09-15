89ae17ac89070e3b4a1c498925463155b59ffc1d
maritzaaazz <maritzaangelinaazzahra@mail.ugm.ac.id>
2023-09-16 01:39:25 +0700

    first commit

---
+++ app/Http/Controllers/BukuController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Buku;
+
+
+class BukuController extends Controller
+{
+    //fungsi index
+    public function index(){
+        $data_buku = Buku::all()->sortByDesc('id');
+        $no = 0;
+        $jumlahData = Buku::all()->count('id');
+        $totalHarga = Buku::all()->sum('harga');
+        return view('buku', compact('data_buku', 'no', 'jumlahData', 'totalHarga')); 
+    }
+}
---
+++ resources/views/buku.blade.php
@@ -0,0 +1,24 @@
+<table border="1 px" class="table table-striped">
+    <thead>
+        <th>id</th>
+        <th>Judul Buku</th>
+        <th>Penulis</th>
+        <th>Harga</th>
+        <th>Tgl. Terbit</th>
+        <th>Aksi</th>
+    </thead>
+    <tbody>
+        @foreach ($data_buku as $buku)
+            <tr>
+                <td>{{ ++$no }}</td>
+                <td>{{ $buku->judul }}</td>
+                <td>{{ $buku->penulis }}</td>
+                <td>{{ "Rp ".number_format($buku->harga, 2, ',', ',') }}</td>
+                <td>{{ date('d/m/Y', strtotime($buku->tgl_terbit)) }}</td>
+            </tr>
+        @endforeach
+    </tbody>
+</table>
+<h3> <p>Jumlah data = {{ $jumlahData }}</p> </h3>
+<h3> <p>Total harga = {{ "Rp ".$totalHarga . ",00"}}</p> </h3>
+
